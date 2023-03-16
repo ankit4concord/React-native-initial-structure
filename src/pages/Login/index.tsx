b25b35a2f778f244ffbb7a0a81f9b71fa6185071
Amol Pokale <>
2023-03-16 20:34:20 +0530

    husky setup

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,34 +1,8 @@
-import {
-  getRequest,
-  postRequest,
-  setAuthorizationToken,
-} from "../../utils/axios";
+import React from 'react';
+import { Button } from 'react-native';
 
-import { Button } from "react-native";
-import api_endpoints from "../../utils/api_endpoints";
-import { useEffect } from "react";
-
-const Login = ({ navigation }: any) => {
-  const getEntries = async () => {
-    const loginData = {
-      inputParams: {
-        badgeNumber: 1000001203,
-        warehouseName: "MAIN",
-        deviceName: "SWLP128-KRISHNA",
-        deviceIP: "fe80::f162:d421:4a2b:ceb2%22",
-        appVersion: "1.2.5.6",
-      },
-    };
-    const response = await postRequest(api_endpoints.userLogin, loginData);
-    console.log(response?.data, "response");
-  };
-
-  useEffect(() => {
-    // setAuthorizationToken("Hello123");
-    getEntries();
-  }, []);
-
-  return <Button title="Login" onPress={() => navigation.navigate("Home")} />;
-};
+function Login({ navigation }: any) {
+  return <Button title="Login" onPress={() => navigation.navigate('Home')} />;
+}
 
 export default Login;
